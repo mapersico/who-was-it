c76959b379681b72c95bbcb8227f4a22e79d8202
Marco Pérsico <marco.persico.94@gmail.com>
2025-06-04 08:36:15 -0300

    Moving to next.js

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { CompareTitlesPageComponent } from './features/compare-titles/compare-titles-page/compare-titles-page.component';
-
-export const routes: Routes = [
-  {
-    path: '',
-    component: CompareTitlesPageComponent,
-  },
-];
