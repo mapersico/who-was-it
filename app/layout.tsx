4f9c206730371ddcdd889fe015ef2071318478a2
Marco Pérsico <marco.persico.94@gmail.com>
2025-06-11 11:24:57 -0300

    Added metadata tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,10 +12,31 @@ const geistSans = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "Who was it?!",
-  description: "Compare the cast of movies and TV shows",
+  title: "Compare Titles | WhoWasIt",
+  description: "Compare the cast of movies and TV shows with WhoWasIt!",
+  openGraph: {
+    title: "Compare TV and Movies Titles | WhoWasIt?!",
+    description: "Compare the cast of movies and TV shows with WhoWasIt!",
+    url: "https://whowasit.site/compare-titles",
+    siteName: "WhoWasIt",
+    images: [
+      {
+        url: "https://whowasit.site/logo.svg",
+        width: 1200,
+        height: 630,
+        alt: "WhoWasIt Logo",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Compare TV and Movies Titles | WhoWasIt?!",
+    description: "Compare the cast of movies and TV shows with WhoWasIt!",
+    images: ["https://whowasit.site/logo.svg"],
+  },
 };
-
 
 export default function RootLayout({
   children,
